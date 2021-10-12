796e355527162e7358ba8a6ed071076f37a4d0a8
Owen Rummage <owen@rummage.cc>
2021-10-11 22:11:41 -0500

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1a1d24",
+        inndigo: "#1e6feb",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
